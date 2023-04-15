d7ded32a8c40442e727637dbf3cd24866f7fef47
3DPros <3dprosteam@gmail.com>
2023-04-15 02:21:04 -0500

    fix save load, pretty UI

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // script.js
 $(document).ready(function() {
     // Create the grid
-    const rowCount = 20;
+    const rowCount = 30;
     const colCount = 6;
     for (let i = 0; i < rowCount; i++) {
         const gridRow = $('<div class="grid-row"></div>');
         $('.grid-container').append(gridRow);
       
-        const labelCell = $(`<input type="text" class="label-cell" teacher-data-cell-id="${i}">`);
+        const labelCell = $(`<input type="text" class="teacher-label-cell" teacher-data-cell-id="${i}">`);
         gridRow.append(labelCell);
       
         for (let j = 0; j < colCount; j++) {
@@ -56,7 +56,7 @@ function addNewRow() {
     const newRowColor = randomPastelColor(rowIndex);
     const newRow = `
       <div class="row" data-row-id="${newRowId}" data-row-color="${newRowColor}">
-        <input type="text" id="input-${newRowId}" placeholder="Card text" style="background-color: ${newRowColor};"> <!-- Add the background-color style here -->
+        <input type="text" id="input-${newRowId}" placeholder="Enter Name..." style="background-color: ${newRowColor};"> <!-- Add the background-color style here -->
         <button class="add-card" data-input-id="input-${newRowId}">Add Section</button>
         <span class="card-count" data-count-for="input-${newRowId}">0</span>
       </div>`;
@@ -114,7 +114,37 @@ $('body').on('click', '.add-card', function() {
     addNewRow();
   });
 
+  $('#scoot').on('click', function() {
+    shiftCardsToLeft();
+  });
 });
+
+// Add this function to script.js
+function shiftCardsToLeft() {
+  $('.grid-row').each(function() {
+    const gridCells = $(this).find('.grid-cell');
+    let emptyIndex = -1;
+    for (let i = 0; i < gridCells.length; i++) {
+      const gridCell = gridCells.eq(i);
+      const card = gridCell.find('.card');
+      if (card.length) {
+        if (emptyIndex !== -1) {
+          gridCells.eq(emptyIndex).append(card);
+          emptyIndex++;
+          gridCell.addClass('empty');
+        } else {
+          gridCell.removeClass('empty');
+        }
+      } else {
+        if (emptyIndex === -1) {
+          emptyIndex = i;
+        }
+        gridCell.addClass('empty');
+      }
+    }
+  });
+}
+
 
 
   // Add this function to script.js to update the card count
@@ -163,11 +193,35 @@ $('body').on('click', '.add-card', function() {
   
   // Update the makeCardsDraggable function in script.js to refresh card counts when a card is dropped
   function makeCardsDraggable() {
-    // ...
+    $('.card').draggable({
+      containment: '.grid-container',
+      revert: 'invalid',
+      zIndex: 100,
+      start: function(event, ui) {
+        ui.helper.data('originalPosition', ui.helper.position());
+      }
+    });
+  
+    initializeDroppableGridCells();
+  }
+  
+
+
+  function initializeDroppableGridCells() {
     $('.grid-cell').droppable({
-      // ...
+      accept: '.card',
       drop: function(event, ui) {
-        // ...
+        const droppedCard = ui.helper;
+        const targetCell = $(this);
+        const sourceCell = droppedCard.parent();
+        const existingCard = targetCell.find('.card');
+  
+        if (existingCard.length) {
+          sourceCell.append(existingCard);
+          existingCard.css(droppedCard.data('originalPosition'));
+        }
+  
+        targetCell.append(droppedCard);
         droppedCard.css({top: 0, left: 0});
   
         const inputId = droppedCard.data('input-id');
@@ -176,186 +230,118 @@ $('body').on('click', '.add-card', function() {
     });
   }
 
+  
+
   // Add this function to script.js
 function randomPastelColor(index) {
     const hue = Math.floor(Math.random() * 360);
-    const pastelColor = `hsl(${index * 30}, ${80/(index / 30 + 1)}%, 90%)`;
+    const pastelColor = `hsl(${index * 80}, 80%, 90%)`;
     return pastelColor;
   }
-  
+
 // Add this event handler to script.js for saving the configuration to a file
 $('#save-to-file').on('click', function() {
-    const rowsData = $('.row').map(function() {
-      const rowId = $(this).attr('data-row-id');
-      const rowColor = $(this).attr('data-row-color');
-      const inputText = $(`#input-${rowId}`).val();
-      return { rowId, rowColor, inputText };
-    }).toArray();
-  
-    const cardsData = $('.card').map(function() {
-      const cardId = $(this).attr('data-card-id');
-      const rowId = $(this).attr('data-row-id');
-      const cardText = $(this).text();
-      const position = $(this).parent().index();
-      return { cardId, rowId, cardText, position };
-    }).toArray();
-  
-    const configuration = { rowsData, cardsData };
-    const configurationString = JSON.stringify(configuration);
-    const configurationBlob = new Blob([configurationString], { type: 'application/octet-stream' });
-  
-    // Create a temporary link and trigger a download
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(configurationBlob);
-    link.download = 'gridCardAppConfiguration.bin';
-    link.style.display = 'none';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  });
+  const rowsData = $('.row').map(function() {
+    const rowId = $(this).attr('data-row-id');
+    const rowColor = $(this).attr('data-row-color');
+    const inputText = $(`#input-${rowId}`).val();
+    return { rowId, rowColor, inputText };
+  }).toArray();
 
-// Add this event handler to script.js for loading the configuration from a file
+  const cardsData = $('.card').map(function() {
+    const cardId = $(this).attr('data-card-id');
+    const rowId = $(this).attr('data-row-id');
+    const cardText = $(this).text();
+    const position = $(this).parent().index();
+    return { cardId, rowId, cardText, position };
+  }).toArray();
+
+  const labelCellData = $('.teacher-label-cell, .control-label-cell').map(function() {
+    const cellId = $(this).attr('teacher-data-cell-id');
+    const cellValue = $(this).val();
+    return { cellId, cellValue };
+  }).toArray();
+
+  const configuration = { rowsData, cardsData, labelCellData };
+  const configurationString = JSON.stringify(configuration);
+  const configurationBlob = new Blob([configurationString], { type: 'application/octet-stream' });
+
+  // Create a temporary link and trigger a download
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(configurationBlob);
+  link.download = 'schedule.bin';
+  link.style.display = 'none';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+
+
 $('#load-from-file').on('click', function() {
-    $('#load-file-input').click();
-  });
-  
-  // Add this event handler to script.js for processing the selected file
-  $('#load-file-input').on('change', function(event) {
-    const file = event.target.files[0];
-  
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        try {
-          const configurationString = e.target.result;
-          const configuration = JSON.parse(configurationString);
-          const { rowsData, cardsData } = configuration;
-  
-          // Remove existing rows and cards
-          $('.row').remove();
-          $('.card').remove();
-  
-          // Load rows
-          for (const rowData of rowsData) {
-            const newRow = `
-              <div class="row" data-row-id="${rowData.rowId}" data-row-color="${rowData.rowColor}">
-                <input type="text" id="input-${rowData.rowId}" value="${rowData.inputText}" style="background-color: ${rowData.rowColor};">
-                <button class="add-card" data-input-id="input-${rowData.rowId}">Add Card</button>
-                <span class="card-count" data-count-for="${rowData.rowId}">0</span>
-              </div>`;
-            $('.rows-container').append(newRow);
+  $('#load-file-input').click();
+});
+$('#load-file-input').on('change', function(event) {
+  const file = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      try {
+        const configurationString = e.target.result;
+        const configuration = JSON.parse(configurationString);
+        const { rowsData, cardsData, labelCellData } = configuration;
+
+        // Remove existing rows and cards
+        $('.row').remove();
+        $('.card').remove();
+
+        // Update label cells
+        for (const labelCellDatum of labelCellData) {
+          const labelCell = $(`.teacher-label-cell[teacher-data-cell-id="${labelCellDatum.cellId}"], .control-label-cell[teacher-data-cell-id="${labelCellDatum.cellId}"]`);
+          if (labelCell.length) {
+            labelCell.val(labelCellDatum.cellValue);
+          } else {
+            const newLabelCell = $(`<input type="text" class="teacher-label-cell" teacher-data-cell-id="${labelCellDatum.cellId}" value="${labelCellDatum.cellValue}">`);
+            $('.grid-labels').append(newLabelCell);
           }
-  
-          // Load cards
-          for (const cardData of cardsData) {
-            const rowColor = $(`.row[data-row-id="${cardData.rowId}"]`).attr('data-row-color');
-            const newCard = $(`
-              <div class="card" data-card-id="${cardData.cardId}" data-row-id="${cardData.rowId}" style="background-color: ${rowColor};">
-                ${cardData.cardText}
-              </div>
-            `);
-            $('.grid-cell').eq(cardData.position).append(newCard);
-  
-            // Update card count
-            const countElement = $(`.card-count[data-count-for="${cardData.rowId}"]`);
-            const currentCount = parseInt(countElement.text());
-            countElement.text(currentCount + 1);
-          }
-  
-          // Make cards draggable again
-          makeCardsDraggable();
-  
-          alert('Configuration loaded!');
-        } catch (error) {
-          alert('Error loading configuration. Please make sure the selected file is valid.');
         }
-      };
-      reader.readAsText(file);
-    }
-  });
-  
 
+        // Load rows
+        for (const rowData of rowsData) {
+          const labelCellValue = $(`.teacher-label-cell[teacher-data-cell-id="${rowData.rowId}"]`).val();
+          const labelCell = $(`<input type="text" class="control-label-cell" teacher-data-cell-id="${rowData.rowId}" value="${labelCellValue}">`);
+          const row = $('<div>', {class: 'row', 'data-row-id': rowData.rowId, 'data-row-color': rowData.rowColor}).append(
+            $('<input>', {type: 'text', id: `input-${rowData.rowId}`, value: rowData.inputText, style: `background-color: ${rowData.rowColor};`}),
+            $('<button>', {class: 'add-card', 'data-input-id': `input-${rowData.rowId}`, text: 'Add Card'}),
+            $('<span>', {class: 'card-count', 'data-count-for-input': `input-${rowData.rowId}`, text: '0'})
+            );
+          $('.rows-container').append(row);
+        }
 
+        // Load cards
+        for (const cardData of cardsData) {
+          const rowColor = $(`.row[data-row-id="${cardData.rowId}"]`).attr('data-row-color');
+          const newCard = $(`
+            <div class="card" data-card-id="${cardData.cardId}" data-row-id="${cardData.rowId}" style="background-color: ${rowColor};">
+              ${cardData.cardText}
+            </div>
+          `);
+          $('.grid-cell').eq(cardData.position - 1).append(newCard);
 
-
-
-
-
-
-
-
-
-
-
-
-
-  // Add this event handler to script.js for saving the configuration
-$('#save-configuration').on('click', function() {
-    const rowsData = $('.row').map(function() {
-      const rowId = $(this).attr('data-row-id');
-      const rowColor = $(this).attr('data-row-color');
-      const inputText = $(`#input-${rowId}`).val();
-      return { rowId, rowColor, inputText };
-    }).toArray();
-  
-    const cardsData = $('.card').map(function() {
-      const cardId = $(this).attr('data-card-id');
-      const rowId = $(this).attr('data-row-id');
-      const cardText = $(this).text();
-      const position = $(this).parent().index();
-      return { cardId, rowId, cardText, position };
-    }).toArray();
-  
-    const configuration = { rowsData, cardsData };
-    localStorage.setItem('gridCardAppConfiguration', JSON.stringify(configuration));
-    alert('Configuration saved!');
-  });
-  
-  // Add this event handler to script.js for loading the configuration
-  $('#load-configuration').on('click', function() {
-    const configurationString = localStorage.getItem('gridCardAppConfiguration');
-  
-    if (configurationString) {
-      const configuration = JSON.parse(configurationString);
-      const { rowsData, cardsData } = configuration;
-  
-      // Remove existing rows and cards
-      $('.row').remove();
-      $('.card').remove();
-  
-      // Load rows
-      for (const rowData of rowsData) {
-        const newRow = `
-          <div class="row" data-row-id="${rowData.rowId}" data-row-color="${rowData.rowColor}">
-            <input type="text" id="input-${rowData.rowId}" value="${rowData.inputText}" style="background-color: ${rowData.rowColor};">
-            <button class="add-card" data-input-id="input-${rowData.rowId}">Add Card</button>
-            <span class="card-count" data-count-for="${rowData.rowId}">0</span>
-          </div>`;
-        $('.rows-container').append(newRow);
+          // Update card count
+          const countElement = $(`.card-count[data-count-for-input="input-${cardData.rowId}"]`);
+          const currentCount = parseInt(countElement.text());
+          countElement.text(currentCount + 1);
+        }
+        makeCardsDraggable()
+        initializeDroppableGridCells()
+      } catch (error) {
+        console.error(error);
+        alert(`Error loading configuration: ${error.message}`);
       }
-  
-      // Load cards
-      for (const cardData of cardsData) {
-        const rowColor = $(`.row[data-row-id="${cardData.rowId}"]`).attr('data-row-color');
-        const newCard = $(`
-          <div class="card" data-card-id="${cardData.cardId}" data-row-id="${cardData.rowId}" style="background-color: ${rowColor};">
-            ${cardData.cardText}
-          </div>
-        `);
-        $('.grid-cell').eq(cardData.position).append(newCard);
-  
-        // Update card count
-        const countElement = $(`.card-count[data-count-for="${cardData.rowId}"]`);
-        const currentCount = parseInt(countElement.text());
-        countElement.text(currentCount + 1);
-      }
-  
-      // Make cards draggable again
-      makeCardsDraggable();
-  
-      alert('Configuration loaded!');
-    } else {
-      alert('No saved configuration found.');
-    }
-  });
-  
+    };
+    reader.readAsText(file);
+  }
+});
+
